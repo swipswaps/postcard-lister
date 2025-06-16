d35264f98aa55c8bfc717cad99d2ac019bdba48c
paulcarl <paul@paulcarl.com>
2025-06-16 16:22:20 -0400

    Updated prompt and options for Litho vs. RPPC. Also improved the UI/description for the branded image URL setting. Added setting for store category ID number.

--- core/csv_generator.py
+++ core/csv_generator.py
@@ -38,7 +38,7 @@ def fill_row(headers: list[str], metadata: dict, front_url: str, back_url: str, 
     row['C:Type'] = metadata.get("Type", '')
     row['C:Posted Condition'] = metadata.get("Posted Condition", '')
     row['C:Era'] = metadata.get("Era", '')
-    row['Store category'] = "4231764019"
+    row['Store category'] = settings.get('store_category_id', '')
     row['Description'] = f"<h1>{metadata.get('Title', '')}</h1><br><p>You will receive the exact postcard in the scans. Please view the scans and message me if you have any questions because I can usually respond within minutes.</p><br>{metadata.get('Description', '')}{settings.get('custom_html', '').format(**metadata)}"
 
     return pd.Series(row)
